22fb90245d9bfaa2b301632bea41f678b88c9ecb
Adrian Bautista <adrianbautista8@gmail.com>
2013-12-15 16:46:00 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArtScrapper::Application.config.secret_key_base = '655da0ee2b03ed52b95aa7c98319670a10fcab01cedb2daca40449e4944f09314f78905dab69e4811cff2acfaf473c52a352868db588c87777e8cfd3d7097c6b'
